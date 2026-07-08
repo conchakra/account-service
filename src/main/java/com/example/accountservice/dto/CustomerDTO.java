248fc4afb508cbfb61a66d2caafccb8c5ca6d1e6
Sinchana <tssinchana@gmail.com>
2026-07-08 10:30:58 +0530

    Removed duplicate CustomerDto.java

--- src/main/java/com/example/accountservice/dto/CustomerDTO.java
+++ src/main/java/com/example/accountservice/dto/CustomerDTO.java
@@ -1,6 +1,5 @@
 package com.example.accountservice.dto;
 
-import com.example.accountservice.dto.CustomerDTO;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
